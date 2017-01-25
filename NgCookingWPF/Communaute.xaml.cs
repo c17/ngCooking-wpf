3053406535e31006c24308bad47b57ea313a428a
WilkoJ <perruchon.wilko.isart@gmail.com>
2017-01-25 18:55:02 +0100

    HyperLink ajouté pour redirection vers les pages.

--- NgCookingWPF/Communaute.xaml.cs
+++ NgCookingWPF/Communaute.xaml.cs
@@ -12,11 +12,31 @@ namespace NgCookingWPF
     public partial class Communaute : Page
     {
         private string _param { get; set; }
+        public Communaute()
+        {
+            InitializeComponent();
+            apis.Client.ApiClient _apiClient = _apiClient = new apis.Client.ApiClient("http://localhost:5000/api");
+
+            List<apis.Client.Models.User> src = _apiClient.Get<List<apis.Client.Models.User>>("community").Result;
+            src = src.OrderBy(o => o.FirstName).ToList();
+            foreach (var item in src)
+            {
+                String url = String.Format("{0}/{1}", "http://localhost:5000/", item.Picture);
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri(url); ;
+                bitmapImage.EndInit();
+                item.Img = bitmapImage;
+            }
+            Users.ItemsSource = src;
+        }
+
+
+        // POUR TESTER AVEC REDIRECTION A LA MAIN
         public Communaute(string param)
         {
             InitializeComponent();
             _param = param;
-            Title.Text = _param;
             apis.Client.ApiClient _apiClient = _apiClient = new apis.Client.ApiClient("http://localhost:5000/api");
 
             List<apis.Client.Models.User> src = _apiClient.Get<List<apis.Client.Models.User>>("community").Result;
